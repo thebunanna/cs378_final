43e870fcbeddce2a8e287b39bd33447c7769cf3a
Simon Pinochet Concha <simon.pinochet@utexas.edu>
2020-11-17 21:31:21 -0600

    Added player character

---
+++ Source/cs378_final/Player/cs378_PlayerController.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "PlayerCharacter.h"
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "cs378_PlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CS378_FINAL_API Acs378_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	Acs378_PlayerController();
+
+protected:
+	virtual void SetupInputComponent() override;
+
+public:
+	void Forward(float value);
+	void Right(float value);
+	void CameraY(float value);
+	void CameraX(float value);
+
+	void Attack();
+	void Pickup();
+};
