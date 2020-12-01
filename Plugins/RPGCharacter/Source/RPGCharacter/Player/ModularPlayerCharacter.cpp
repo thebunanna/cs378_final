0745654574e95777bd582d171289276a4a471b9e
Jared Hayes <jaredhayes@Jareds-MacBook-Pro.local>
2020-11-30 20:31:18 -0600

    fixed takedamage in rpgchar

--- Plugins/RPGCharacter/Source/RPGCharacter/Player/ModularPlayerCharacter.cpp
+++ Plugins/RPGCharacter/Source/RPGCharacter/Player/ModularPlayerCharacter.cpp
@@ -137,7 +137,7 @@ void AModularPlayerCharacter::Block()
 
 }
 
-void AModularPlayerCharacter::TakeDamage(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+void AModularPlayerCharacter::TakeCharDamage(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 	if (OtherActor->IsA(AWeapon::StaticClass()))
 	{
